4e3acc9b702ccf3e2daba8ad3d1cfd1a7e8b06ae
Mai V. Xuan Vuong <xuannvuongg@gmail.com>
2024-06-13 16:30:03 +0700

    feat : new table

--- src/models/sql/create-tables.sql
+++ src/models/sql/create-tables.sql
@@ -1,4 +1,11 @@
-
+-- Tạo bảng category
+CREATE TABLE IF NOT EXISTS category (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 -- Tạo bảng products
 CREATE TABLE IF NOT EXISTS products (
@@ -6,28 +13,21 @@ CREATE TABLE IF NOT EXISTS products (
     name VARCHAR(255) NOT NULL,
     description TEXT,
     price NUMERIC(10, 2) NOT NULL,
+    discount FLOAT,
+    stock INTEGER DEFAULT 0,
     images VARCHAR(255)[] DEFAULT ARRAY[]::VARCHAR(255)[],
+    category_id SERIAL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES category (id)
 );
-
--- Tạo bảng carts
-CREATE TABLE IF NOT EXISTS carts (
-    id VARCHAR(255) NOT NULL PRIMARY KEY,
-    user_id VARCHAR(255) NOT NULL, -- Liên kết ngoại với bảng users
-    product_id VARCHAR(255) NOT NULL, -- Liên kết ngoại với bảng products
-    quantity INTEGER DEFAULT 1,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
 
 -- Tạo bảng users
 CREATE TABLE IF NOT EXISTS users (
     id VARCHAR(255) NOT NULL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     username VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(50) UNIQUE ,
+    phone_number VARCHAR(50) UNIQUE,
     password VARCHAR(255) NOT NULL,
     date_of_birth DATE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -35,10 +35,24 @@ CREATE TABLE IF NOT EXISTS users (
     email_verification_token TEXT,
     forgot_password_token TEXT,
     verify VARCHAR(100),
-    avatar VARCHAR(255),
-    cart_id VARCHAR(255) NOT NULL -- Liên kết ngoại với bảng carts
+    avatar VARCHAR(255)
 );
 
+-- Tạo bảng carts
+CREATE TABLE IF NOT EXISTS carts (
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL, -- Liên kết ngoại với bảng users
+    product_id VARCHAR(255) NOT NULL, -- Liên kết ngoại với bảng products
+    quantity INTEGER DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (product_id) REFERENCES products (id)
+);
+
+
+
+-- Tạo bảng refresh_tokens
 CREATE TABLE IF NOT EXISTS refresh_tokens (
     id SERIAL PRIMARY KEY,
     token TEXT NOT NULL,
@@ -46,9 +60,5 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
     user_id VARCHAR(255) NOT NULL,
     iat VARCHAR(255),
     exp VARCHAR(255),
-    CONSTRAINT fk_user
-        FOREIGN KEY(user_id) 
-        REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
-
-
