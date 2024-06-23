18ab5cd3c62815d661c8dc817066ee07367ff730
Mai V. Xuan Vuong <xuannvuongg@gmail.com>
2024-06-23 20:46:01 +0700

    feat: Add created_at and updated_at columns to carts table

--- src/models/sql/create-tables.sql
+++ src/models/sql/create-tables.sql
@@ -46,6 +46,8 @@ CREATE TABLE IF NOT EXISTS carts (
     quantity INTEGER DEFAULT 1,
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (product_id) REFERENCES products (id)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
 
 
