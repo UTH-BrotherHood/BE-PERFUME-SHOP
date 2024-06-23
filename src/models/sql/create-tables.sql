3ce1f259bb94d4beb8a4b90d8de0057b95d7b05c
Mai V. Xuan Vuong <xuannvuongg@gmail.com>
2024-06-23 20:50:05 +0700

    chore: Remove trailing comma in create-tables.sql

--- src/models/sql/create-tables.sql
+++ src/models/sql/create-tables.sql
@@ -45,9 +45,9 @@ CREATE TABLE IF NOT EXISTS carts (
     product_id VARCHAR(255) NOT NULL, 
     quantity INTEGER DEFAULT 1,
     FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (product_id) REFERENCES products (id)
+    FOREIGN KEY (product_id) REFERENCES products (id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
